a5908fc673a30fb5b40cce59133e550a9efea3dd
Arnoldosmium <eaglemewpsy@gmail.com>
2018-09-10 21:37:50 -0400

    add skip, limit, and related methods

--- pydragon/chainer.py
+++ pydragon/chainer.py
@@ -1,4 +1,4 @@
-from itertools import chain
+from itertools import chain, islice, dropwhile, takewhile, starmap
 from functools import reduce
 from .util import streamify
 
@@ -12,10 +12,10 @@ class Dragon(object):
             return chain.from_iterable(map(streamify, stream_or_things))
 
     def __init__(self, *stream_or_things):
-        self._stream = Dragon.prepare_stream(*stream_or_things)
+        self.__stream = Dragon.prepare_stream(*stream_or_things)
 
     def __next__(self):
-        return next(self._stream)
+        return next(self.__stream)
 
     def next(self):
         return self.__next__()
@@ -24,7 +24,7 @@ class Dragon(object):
         return self
 
     def add(self, *stream_or_things):
-        self._stream = Dragon.prepare_stream(self._stream, *stream_or_things)
+        self.__stream = Dragon.prepare_stream(self.__stream, *stream_or_things)
         return self
 
     def chain(self, *args):
@@ -34,15 +34,15 @@ class Dragon(object):
         return self.add(*args)
 
     def map(self, func):
-        self._stream = map(func, self._stream)
+        self.__stream = map(func, self.__stream)
         return self
 
     def flat_map(self, func):
-        self._stream = chain.from_iterable(map(streamify, map(func, self._stream)))
+        self.__stream = chain.from_iterable(map(streamify, map(func, self.__stream)))
         return self
 
     def filter(self, func):
-        self._stream = filter(func, self._stream)
+        self.__stream = filter(func, self.__stream)
         return self
 
     def exclude(self, func):
@@ -56,9 +56,9 @@ class Dragon(object):
 
     def reduce(self, reducer, initial_value=None):
         if initial_value is not None:
-            return reduce(reducer, self._stream, initial_value)
+            return reduce(reducer, self.__stream, initial_value)
         else:
-            return reduce(reducer, self._stream)
+            return reduce(reducer, self.__stream)
 
     def foreach(self, func):
         for element in self:
@@ -74,4 +74,26 @@ class Dragon(object):
             return min(self)
         return min(self, key=key)
 
+    def limit(self, num):
+        if num > 0:
+            self.__stream = islice(self.__stream, num)
+        return self
 
+    def takewhile(self, func):
+        self.__stream = takewhile(self.__stream, func)
+        return self
+
+    def cutoff_if(self, func):
+        return self.takewhile(func)
+
+    def skip(self, num):
+        if num > 0:
+            self.__stream = islice(self.__stream, num, None)
+        return self
+
+    def dropwhile(self, func):
+        self.__stream = dropwhile(self.__stream, func)
+        return self
+
+    def skip_util(self, func):
+        return self.dropwhile(func)
--- test/test_main.py
+++ test/test_main.py
@@ -28,3 +28,55 @@ def test_basic():
     s5 = Stream(range(10)) \
         .reduce(lambda prev, this: (prev[0] if this % 2 else prev[0] + [this], prev[1] if this % 4 else prev[1] + [this]), ([], []))
     assert s5 == (list(range(0, 10, 2)), list(range(0, 10, 4)))
+
+
+def test_skip():
+    s1 = Stream(range(10)) \
+            .skip(5) \
+            .collect(list)
+    assert s1 == list(range(5, 10))
+
+    s2 = Stream(range(20)) \
+        .skip(5) \
+        .skip(10) \
+        .collect(list)
+    assert s2 == list(range(15, 20))
+
+    s3 = Stream(range(20)) \
+        .filter(lambda x: x % 2) \
+        .skip(15) \
+        .collect(list)
+    assert s3 == []
+
+
+def test_limit():
+    s1 = Stream(range(10)) \
+        .limit(5) \
+        .collect(list)
+    assert s1 == list(range(5))
+
+    s2 = Stream(range(10)) \
+        .limit(5) \
+        .limit(10) \
+        .collect(list)
+    assert s2 == list(range(5))
+
+    s3 = Stream(range(10)) \
+        .limit(10) \
+        .limit(5) \
+        .collect(list)
+    assert s3 == list(range(5))
+
+
+def test_skip_limit():
+    s1 = Stream(range(20)) \
+        .skip(10) \
+        .limit(5) \
+        .collect(list)
+    assert s1 == list(range(10, 15))
+
+    s2 = Stream(range(20)) \
+        .limit(5) \
+        .skip(10) \
+        .collect(list)
+    assert s2 == []
