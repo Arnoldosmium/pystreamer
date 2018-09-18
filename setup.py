6299800a88b2da642460b11885d2016004b8c2e1
Arnoldosmium <eaglemewpsy@gmail.com>
2018-09-18 00:58:13 -0400

    version?

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pydragon',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='A chainable stream solution',
 
