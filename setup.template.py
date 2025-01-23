2601b1abfff73c347eedb0c98e18e8d11f91b44f
Arnoldosmium <contact@arnoldlin.tech>
2025-01-22 20:51:25 -0500

    Update proper metadata in setup.py

--- setup.template.py
+++ setup.template.py
@@ -11,14 +11,14 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="pyStreamer",
+    name="pystreamer",
     version=VERSION,
     description="A lazy evaluating, memory friendly, chainable stream solution",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Arnoldosmium/pystreamer",
     download_url="https://pypi.org/project/pyStreamer/",
-    author="Arnold Lin",
+    author="Arnold Y. Lin",
     author_email="contact@arnoldlin.tech",
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -26,10 +26,10 @@ setup(
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="stream generator chain",  # Optional
     packages=find_packages(exclude=["contrib", "docs", "test"]),
